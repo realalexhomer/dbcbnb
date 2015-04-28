0b04878eb761c1f266f9159a693e171dd7ff488f
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-04-28 16:30:16 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Dbcbnb, $*/
+
+
+window.Dbcbnb = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Dbcbnb.init();
+});
